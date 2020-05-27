313d942e470cc197e7b57a872e3aef8fdebb879e
Harsha <harshazawar92@gmail.com>
2020-05-28 00:25:18 +0530

    Gives information of weather

---
+++ app/src/main/java/cz/martykan/forecastie/Constants.java
@@ -0,0 +1,9 @@
+package cz.martykan.forecastie;
+
+public class Constants {
+    public static final String  DEFAULT_CITY = "Pune";
+    public static final String  DEFAULT_LAT = "18.519569";
+    public static final String  DEFAULT_LON = "73.855347";
+    public static final String DEFAULT_CITY_ID = "1259229";
+    public static final int DEFAULT_ZOOM_LEVEL = 7;
+}
